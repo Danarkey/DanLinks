fafd54bd322ae27ddeb58fcb0fb8c4571d354c34
Danarkey <138664486+Danarkey@users.noreply.github.com>
2026-09-22 09:52:28 +1000

    Added MC

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
  * MANUAL CONFIG
  *************************************************/
 
-const DEFAULT_FORMAT = "regmb"; // e.g. "regf", "regh", "regi"
+const DEFAULT_FORMAT = "regmc"; // e.g. "regf", "regh", "regi"
 
 const FORMAT_CONFIG = {
+    regmc: { label: "Regulation M-C", file: "pastesMC.json" },
     regmb: { label: "Regulation M-B", file: "pastesMB.json" },
     regma: { label: "Regulation M-A", file: "pastesMA.json" },
     regi: { label: "Regulation I", file: "pastesI.json" },
